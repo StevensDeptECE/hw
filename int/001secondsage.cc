6b936351a8d511f2138a9d2bef3e323f065f68d1
Dov Kruger <Dov.Kruger@stevens.edu>
2020-12-30 13:07:53 -0500

    first

---
+++ int/001secondsage.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age;
+	cout << "Enter your age in years: ";
+	cin >> age;
+
+	// print out your age in seconds.
+	// find the number where it overflows
+	
+	return 0;
+}
